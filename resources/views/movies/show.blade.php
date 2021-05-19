84da6f2e7de2abf463d4792bca71a1d5e3f83bcf
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-05-19 17:02:28 +0200

    Aggiunto il Regista alle pagine Show

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -7,6 +7,7 @@
 
 @section('content')
     <img class="mb-3" src="{{$movie->cover}}" alt={{$movie->title}}>
+    <h2>{{$movie->director}}</h2>
 	<h2>{{$movie->year}}</h2>
 	<p class="text-justify">{{$movie->plot}}</p>
 	<a href="{{route('movies.index')}}"><button type="button" class="btn btn-primary mb-3">Torna all'home page</button></a>
